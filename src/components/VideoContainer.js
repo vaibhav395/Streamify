c879749e08872de61d9756982ae0916db1459883
vaibhav395 <90473158+vaibhav395@users.noreply.github.com>
2024-10-28 16:36:32 +0530

    Second commit

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
+
 const VideoContainer = ()=>{
+
+    const [videos, setVideos] = useState([]);
+    useEffect(()=>{
+        getVideos();
+    }, [])
+
+    const getVideos = async ()=>{
+        const data = await fetch(YOUTUBE_VIDEOS_API)
+        const json = await data.json()
+        setVideos(json.items)
+    }
     return(
-        <div>
-            video container
+        <div className="flex flex-wrap">
+            {videos.map((video)=>{
+                return <VideoCard key={video.id} info={video}/>
+            })}
         </div>
     )
 }
