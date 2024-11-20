db19e8573704295744e0e093e651bb24a732b353
vaibhav395 <90473158+vaibhav395@users.noreply.github.com>
2024-11-20 23:02:02 +0530

    commited

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ const GOOGLE_API_KEY = "AIzaSyAsHh0Yib7B1juhsl0tOTN7vWGmaDoveN8"
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY
 
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const live_chat_count = 10;
